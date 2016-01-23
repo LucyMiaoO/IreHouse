cff475031a35f115ec09610ac465d0fb95b75a05
Xinyu Miao <hermit412@gmail.com>
2016-01-23 23:40:38 +0000

    fix the bugs

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,16 +1,13 @@
 class Listing < ActiveRecord::Base
   if Rails.env.development?
     has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
+    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   else
     has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
                       :storage => :dropbox,
                       :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
                       :path => ":style/:id_:filename"
+    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   end
-
-  validates :city, :description, :deposit, presence: true
-  validates :deposit, numericality: { greater_than: 0 }
-  validates_attachment_presence :image
-  validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/]
-
+  
 end
