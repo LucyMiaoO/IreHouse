ea041a2e144c09842766814a4bf1eaff2e4eadc5
Hermit <hermit@HermittekiMacBook-Pro.local>
2016-02-02 17:22:39 +0000

    fix bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  #..
   has_many :listings, dependent: :destroy
   has_many :receipts, class_name: "Reservation", foreign_key: "landlord_id"
   has_many :reserves, class_name: "Reservation", foreign_key: "student_id"
